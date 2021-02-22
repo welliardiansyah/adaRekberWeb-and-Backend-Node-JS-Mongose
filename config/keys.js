70cb15767c38a1c2e9be4a3118bee8aa5ac25e2e
welly ardiansyah <68628069+doctorcodes13@users.noreply.github.com>
2021-02-22 10:45:29 +0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/adaRekber",
+  mongoURI: "mongodb://doctercodes_admin:<!#Julli199@>@cluster0-shard-00-00.3rsuv.mongodb.net:27017,cluster0-shard-00-01.3rsuv.mongodb.net:27017,cluster0-shard-00-02.3rsuv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-hmsa31-shard-0&authSource=admin&retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
